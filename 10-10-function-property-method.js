1a587a84b7da59c0ff4c6f3c13ef1b40727bce79
Z20070184 <Joe_Yan@wistron.com>
2021-11-01 10:54:04 +0800

    'update'

--- 10-10-function-property-method.js
+++ 10-10-function-property-method.js
@@ -4,7 +4,7 @@
  * @Autor: Joe Yan
  * @Date: 2021-11-01 10:04:15
  * @LastEditors: Joe Yan
- * @LastEditTime: 2021-11-01 10:07:18
+ * @LastEditTime: 2021-11-01 10:51:24
  */
 function sayName(name) {
     console.log(name);
@@ -21,3 +21,20 @@ function sayHi() {
 console.log(sayName.length);
 console.log(sum.length);
 console.log(sayHi.length);
+
+/** apply 、call 
+ *  作用： 控制函数调用上下文即函数体内this值的能力 
+ */
+
+color = 'red';
+let o = {
+    color: 'blue'
+};
+
+function sayColor() {
+    console.log(this.color);
+}
+
+sayColor();
+
+sayColor.call(o); //red
