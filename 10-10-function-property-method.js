462e3203a12108944c00fc94f1c4439902a83efe
Z20070184 <Joe_Yan@wistron.com>
2021-11-01 10:10:24 +0800

    'update'

---
+++ 10-10-function-property-method.js
@@ -0,0 +1,23 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Autor: Joe Yan
+ * @Date: 2021-11-01 10:04:15
+ * @LastEditors: Joe Yan
+ * @LastEditTime: 2021-11-01 10:07:18
+ */
+function sayName(name) {
+    console.log(name);
+}
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function sayHi() {
+    console.log("hi");
+}
+
+console.log(sayName.length);
+console.log(sum.length);
+console.log(sayHi.length);
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
+<!--
+ * @Description: 
+ * @Version: 
+ * @Autor: Joe Yan
+ * @Date: 2021-11-01 09:46:42
+ * @LastEditors: Joe Yan
+ * @LastEditTime: 2021-11-01 09:56:21
+-->
 # JS-Advanced-Training
-Some code testing in Professional JavaScript for Web Developers, 4th Edition.
+Some code testing in Professional JavaScript for Web Developers, 4th Edition. test!!!
